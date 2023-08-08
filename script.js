20f2212bde18dd1d197c833f750836a2a718bbba
Pie is Spy <95068281+PieIsSpy@users.noreply.github.com>
2023-08-08 20:53:49 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var img = document.createElement('button')
+img.setAttribute('src','cute shiorin.jpg');
+
+const sounds = ['giggle1', 'giggle2', 'giggle3', 'giggle4', 'giggle5', 'giggle6', 'giggle7', 'giggle8', 'giggle9', 'giggle10', 'giggle11', 'giggle12']
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = test
+
+  document.getElementById('buttons').
+  appendChild(btn)
+})
