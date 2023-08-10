d7cd01653c89e009122053737390d10e6834d0ee
Pie is Spy <95068281+PieIsSpy@users.noreply.github.com>
2023-08-10 18:55:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ img.setAttribute('src','cute shiorin.jpg');
 const sounds = ['giggle1', 'giggle2', 'giggle3', 'giggle4', 'giggle5', 'giggle6', 'giggle7', 'giggle8', 'giggle9', 'giggle10', 'giggle11', 'giggle12']
 
 function giggle(sounds) {
-  min =
-  return Math.floor(Math.random() *
+  min = Math.ceil(1);
+  max = Math.floor(12);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
