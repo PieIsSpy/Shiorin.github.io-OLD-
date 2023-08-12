781e908fc02233f8ed85581896f0bdddd3b63a87
Pie is Spy <95068281+PieIsSpy@users.noreply.github.com>
2023-08-12 13:20:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var img = document.createElement('button');
 img.setAttribute('src','cute shiorin.jpg');
 
-const sounds = ['giggle1.mp3', 'giggle2.mp3', 'giggle3.mp3', 'giggle4.mp3', 'giggle5.mp3', 'giggle6.mp3', 'giggle7.mp3', 'giggle8.mp3', 'giggle9.mp3', 'giggle10.mp3', 'giggle11.mp3', 'giggle12.mp3'];
+const sounds = ['giggle01.mp3', 'giggle02.mp3', 'giggle03.mp3', 'giggle04.mp3', 'giggle05.mp3', 'giggle06.mp3', 'giggle07.mp3', 'giggle08.mp3', 'giggle09.mp3', 'giggle10.mp3', 'giggle11.mp3', 'giggle12.mp3'];
 
 function randomGiggles() {
   const audioContent = Math.floor(Math.random() * sounds.length);
